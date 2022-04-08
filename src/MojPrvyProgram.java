ac26a60f195d6f934ebec1c95bfa6fae7781eca3
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-04-08 10:47:46 +0200

    prvy commit

---
+++ src/MojPrvyProgram.java
@@ -0,0 +1,44 @@
+public class MojPrvyProgram {
+	public static void main(String[] args) {
+		//jednoriadkovy komentar
+		//toto je mocny prikaz
+		System.out.println("Ahoj Java");
+		System.out.println("Dnes je piatok");
+		System.out.print("Janko");
+		System.out.println("Hrasko");
+		
+		System.out.println("Ivan\nHrozny");
+		//System.out.print("Hrozny");
+		
+		System.out.println("Fero\tMravec");
+		System.out.println("Adalad\tMiazga");
+		System.out.println("\"Dnes je piatok\"");
+		System.out.println("\\Lukas don\'t know\\");
+		
+		System.out.println(10 - 5);
+		System.out.println("10 - 5");
+		
+		System.out.println("Lukas" + " " +  "Zmuda");
+		//retazec + retazec = retazec
+		System.out.println("10" + "5");
+		System.out.println("10" + 5);
+		
+		System.out.println("10 + 5 = " + (10 + 5));
+		
+		
+		
+		
+		/*
+		 * toto je 
+		 * viacriadkovy 
+		 * komentar
+		 */
+		
+		/**
+		 * javadoc
+		 * komentar
+		 */
+	}
+	
+}
+
